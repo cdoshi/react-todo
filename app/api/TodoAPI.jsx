0c9331c99b789b12b0836bbbb23386b054c21880
Chiran <chiran@tickledmedia.com>
2017-08-28 19:03:59 +0530

    Add filter and test

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -16,5 +16,36 @@ module.exports = {
     } catch (e) {}
 
     return $.isArray(todos) ? todos : [];
+  },
+
+  filterTodos: function(todos, showCompleted, searchText) {
+      var filterTodos = todos;
+
+      // Filter by showCompleted
+      filterTodos = filterTodos.filter((todo) => {
+        return !todo.completed || showCompleted;
+      });
+
+      // Filter by searchText
+      filterTodos = filterTodos.filter((todo) => {
+        if(searchText !== '') {
+          return todo.text.toLowerCase().indexOf(searchText) >= 0
+        } else {
+          return true;
+        }
+      });
+
+      // Sort todos with non-completed first
+      filterTodos.sort((a, b) => {
+        if(!a.completed && b.completed) {
+          return -1;
+        } else if(a.completed && !b.completed) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      return filterTodos;
   }
 };
