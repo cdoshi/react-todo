c5e461ad9f622b2c34a6beafa5041c839c14845f
Chiran <chiran@tickledmedia.com>
2017-09-07 12:11:34 +0530

    Move firebase credentials to environemnt variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
 
+
 try {
   var config = {
-    apiKey: "AIzaSyAOBtXrkpdtOIignUc-Pe2DxCPKEh33olA",
-      authDomain: "chiran-todo-app.firebaseapp.com",
-      databaseURL: "https://chiran-todo-app.firebaseio.com",
-      projectId: "chiran-todo-app",
-      storageBucket: "chiran-todo-app.appspot.com",
-      messagingSenderId: "131186155799"
+    apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch(e) {
